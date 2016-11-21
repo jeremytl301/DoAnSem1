8df2a02ac96ce022d940e20176d6d0171a3fa6da
jeremytl301 <c.lyoko.jeremy@gmail.com>
2016-11-21 20:00:03 +0700

    File Day Du

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/wraper.css"/>
+<link rel="stylesheet" type="text/css" href="css/top.css"/>
+</head>
+<a href="index.php"></a>
+<body>
+
+	<div class="wraper">
+    	<!-- BEGIN TOP -->
+    	<div class="top">
+        	<div class="main_menu">
+            	<ul>
+                	<li><a href=""><b>TOURS</b></a></li>
+                    <li><a href=""><b>NON NƯỚC</b></a></li>
+                    <li><a href=""><b>LOGO</b></a></li>
+                    <li><a href=""><b>SỰ KIỆN</b></a></li>
+                    <li><a href=""><b>CÁI ÔN</b></a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- END TOP -->
+        
+        <!-- BEGIN MENU HIDE -->
+        <div class="menu_hide">
+        </div>
+        <!-- END MENU HIDE -->
+        
+        <!-- BEGIN CONTAINER -->
+        <div class="container">
+        </div>
+        <!-- END CONTAINER -->
+        
+        <!-- BEGIN FOOTER -->
+        <div class="footer">
+        </div>
+        <!-- END FOOTER -->
+    </div>
+
+</body>
+</html>
